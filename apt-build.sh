53f520f6dd23a87d74b92317ce5b1f942c5e44e5
Torrie Fischer <torrie@ripple.com>
2015-04-16 08:36:29 -0700

    apt-build: directory independent

--- apt-build.sh
+++ apt-build.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get install -qq g++-4.8 libboost1.57-all-dev protobuf-compiler libprotobuf-dev libssl-dev gcc-4.8 binutils-gold dh-make scons pkg-config quilt git
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8
 update-alternatives --set gcc /usr/bin/gcc-4.8
-cd /root/apt/
+cd `dirname $0`
 git clone git://github.com/ripple/rippled
 cd rippled
 git archive develop --prefix=rippled-0.28/ | xz > /../rippled_0.28.orig.tar.xz
